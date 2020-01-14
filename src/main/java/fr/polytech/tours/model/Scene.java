da49a8f87d64aaf9a0fd3c1ce9f31b681024d5c5
Jydet <craairdu26@gmail.com>
2020-01-14 22:40:05 +0100

    sdfsf

--- src/main/java/fr/polytech/tours/model/Scene.java
+++ src/main/java/fr/polytech/tours/model/Scene.java
@@ -1,13 +1,14 @@
 package fr.polytech.tours.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Getter
 @Entity
-@Inheritance(strategy= InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class Scene implements Versionable<Integer> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +20,7 @@ public abstract class Scene implements Versionable<Integer> {
     private Film film;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "scene")
+    @Setter
     private List<Setup> setups;
 
     @Override
